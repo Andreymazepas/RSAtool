921e82d97bb48565bc30fe88a80fd5b5904c2cc8
Andrey Mazépas <andreymazepas@gmail.com>
2019-12-01 17:22:22 -0300

    Geração de chaves, css novo +todos

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ var appRSA = new Vue({
     chave: 'Chave',
     codigo: "",
     decriptado: "",
+    p: null,
+    q: null,
+    phi: null
   },
   methods: {
     encryptMessage: function () {
@@ -39,69 +42,77 @@ var appRSA = new Vue({
         }
 
     	// this.message = this.message.split('').reverse().join('')
+    },
+    isPrimo: num => {
+      for(let i = 2, s = Math.sqrt(num); i <= s; i++)
+          if(num % i === 0) return false; 
+      return num > 1;
+  },
+    GCD: function(e, phi) {
+      let gcd = 0;
+      let a = 0;
+      if(e > phi) {
+        while( e % phi != 0) {
+          a = e%phi;
+          e = phi;
+          phi = a;
+        }
+        gcd = phi;
+      } else {
+        while(phi%e != 0) {
+          a = phi%e;
+          phi = e;
+          e = a;
+        }
+        gcd = e;
+      }
+      return gcd;
+    },
+    encontrarExpoente: (phi, p, q) => {
+      let gcd = 0;
+      let e = 2;
+      while(gcd != 1) {
+        e = e + 1
+        gcd = this.GCD(e, phi);
+        phi = (p-1)*(q-1);
+      }
+      return e;
     }
   }
 })
 
 
 
-
-
-
-
-
-
-
-
-// var app = new Vue({ 
-//     el: '#app',
-//     data: {
-//         message: 'Hello Vue!'
-//     }
-// });
-
-
-// var app2 = new Vue({
-//   el: '#app-2',
-//   data: {
-//     message: 'You loaded this page on ' + new Date().toLocaleString()
-//   }
-// })
-
-
-// var app3 = new Vue({
-//   el: '#app-3',
-//   data: {
-//     seen: true
-//   }
-// })
-
-// var app4 = new Vue({
-//   el: '#app-4',
-//   data: {
-//     todos: [
-//       { text: 'Learn JavaScript' },
-//       { text: 'Learn Vue' },
-//       { text: 'Build something awesome' }
-//     ]
-//   }
-// })
-
-// var app5 = new Vue({
-//   el: '#app-5',
-//   data: {
-//     message: 'Hello Vue.js!'
-//   },
-//   methods: {
-//     reverseMessage: function () {
-//       this.message = this.message.split('').reverse().join('')
-//     }
-//   }
-// })
-
-// var app6 = new Vue({
-//   el: '#app-6',
-//   data: {
-//     message: 'Hello Vue!'
-//   }
-// })
+function extend(E,PHI) {
+  u1 = 1
+  u2 = 0
+  u3 = PHI
+  v1 = 0
+  v2 = 1
+  v3 = E     
+  
+  while (v3 != 0) {
+       q = Math.floor(u3/v3)
+       t1 = u1 - q * v1
+       t2 = u2 - q * v2
+       t3 = u3 - q * v3
+  
+       u1 = v1
+       u2 = v2 
+       u3 = v3
+  
+       v1 = t1
+       v2 = t2
+       v3 = t3
+       z = 1
+  }
+  uu = u1
+  vv = u2
+  
+  if (vv < 0) {
+            inverse = vv + PHI
+  } else {
+       inverse = vv
+  }
+  return inverse
+  }    
