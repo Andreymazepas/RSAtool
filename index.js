1b96c644a4ff7b1ff8ca76796ceac4695a4fee52
Andrey Mazépas <andreymazepas@gmail.com>
2019-12-02 15:53:14 -0300

    quebrei de algum jeito

--- index.js
+++ index.js
@@ -1,82 +1,59 @@
+const GCD = function(e, phi) {
+  let gcd = 0;
+  let a = 0;
+  if(e > phi) {
+    while( e % phi != 0) {
+      a = e%phi;
+      e = phi;
+      phi = a;
+    }
+    gcd = phi;
+  } else {
+    while(phi%e != 0) {
+      a = phi%e;
+      phi = e;
+      e = a;
+    }
+    gcd = e;
+  }
+  return gcd;
+}
+
 var appRSA = new Vue({
   el: '#app-RSA',
   data: {
-    message: 'Mensagem',
+    message: '',
+    message2: '',
     chave: 'Chave',
     codigo: "",
     decriptado: "",
     p: null,
     q: null,
-    phi: null
+    phi: null,
+    expoente: null,
+    step: 0,
+    d: null,
   },
   methods: {
-    encryptMessage: function () {
-    	P = 17;
-    	Q = 29;
-
-    	N = P * Q; // 493
-    	Z = (P-1) * (Q-1); // 448
-
-    	D = 443; // Primo em relação a Z
-    	E = -1;
-    	for(i = 0; i <= Z; i++){
-    		if ((i * D) % Z == 1) E = i;
-    	}
-
-    	console.log("P: " + P);
-        console.log("Q: " + Q);
-        console.log(" ");
-        console.log("N: " + N);
-        console.log("Z: " + Z);
-        console.log(" ");
-        console.log("D: " + D);
-        console.log("E: " + E);
-        console.log(" ");
-
-        for (i = 0; i < this.message.length; i++){
-        	this.codigo += String.fromCharCode( this.message.charCodeAt(i) ^ E % N );
-        }
-
-        for (i = 0; i< this.codigo.length; i++){
-        	this.decriptado += String.fromCharCode( this.codigo.charCodeAt(i) ^ D % N )
-        }
-
-    	// this.message = this.message.split('').reverse().join('')
-    },
     isPrimo: num => {
       for(let i = 2, s = Math.sqrt(num); i <= s; i++)
           if(num % i === 0) return false; 
       return num > 1;
   },
-    GCD: function(e, phi) {
-      let gcd = 0;
-      let a = 0;
-      if(e > phi) {
-        while( e % phi != 0) {
-          a = e%phi;
-          e = phi;
-          phi = a;
-        }
-        gcd = phi;
-      } else {
-        while(phi%e != 0) {
-          a = phi%e;
-          phi = e;
-          e = a;
-        }
-        gcd = e;
-      }
-      return gcd;
-    },
+    
     encontrarExpoente: (phi, p, q) => {
       let gcd = 0;
       let e = 2;
+      phi = (p-1)*(q-1);
       while(gcd != 1) {
         e = e + 1
-        gcd = this.GCD(e, phi);
+        gcd = GCD(e, phi);
         phi = (p-1)*(q-1);
       }
-      return e;
+      appRSA.expoente = e;
+      appRSA.d = extend(e, phi);
+      appRSA.step = 1;
+      return;
     }
   }
 })
@@ -116,3 +93,5 @@ function extend(E,PHI) {
   }
   return inverse
   }    
+
+
